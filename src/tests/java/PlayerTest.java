70b816f628dbaa9e32ed47722654c00b35d81622
Slav <Slav@DESKTOP-DB0IU3A>
2018-03-19 00:23:27 +0000

    Finished up tests for Player and Players classes.

--- src/tests/java/PlayerTest.java
+++ src/tests/java/PlayerTest.java
@@ -1,40 +1,113 @@
 package tests.java;
 
-
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import main.java.LetterCryptogram;
+import main.java.NumberCryptogram;
 import main.java.Player;
 import main.java.Players;
 
 public class PlayerTest {
-	
+
+	private Player john;
+	private Player james;
+	private Players array_list;
+
+	@AfterEach
+	protected void tearDown() {
+		john = null;
+		james = null;
+		array_list = null;
+	}
+
 	@Test
 	public void getNameReturnsCorrectMessage() {
-        Player john = new Player("John");
-        assertTrue(john.getName().equals("John"));
-    }
-	
-	@Test
-	public void findPlayerReturnsCorrectMessage()
-	{
-		Players array_list = new Players();
-		Player john = new Player("John");
-		array_list.addPlayer(john);
-		 assertTrue(array_list.findPlayer("John").getName().equals(john.getName()));
+		 john = new Player("John");
+		 
+		assertTrue(john.getName().equals("John"));
 	}
-	
-	
-	//Needs work still
+
 	@Test
-	public void removePlayerRemovesCorrectPlayer()
-	{
-		Players array_list = new Players();
-		Player john = new Player("John");
+	public void findPlayerReturnsCorrectMessage() {
+		 array_list = new Players();
+		 john = new Player("John");
+		 
+		array_list.addPlayer(john);
+		assertTrue(array_list.findPlayer("John").getName().equals(john.getName()));
+	}
+
+	@Test
+	public void removePlayerRemovesCorrectPlayer() {
+		 array_list = new Players();
+		 john = new Player("John");
+		 
 		array_list.addPlayer(john);
 		array_list.removePlayer("John");
-		assertTrue(array_list.findPlayer("John").equals(null));
+		assertNull(array_list.findPlayer("John"));
+	}
+
+	@Test
+	public void getAllPlayerAccuraciesReturnsCorrectValue() {
+		 array_list = new Players();
+		 john = new Player("John");
+		 james = new Player("James");
+
+		array_list.addPlayer(john);
+		array_list.addPlayer(james);
+
+		ArrayList<Integer> accuracy_test = array_list.getAllPlayersAccuracies();
+		assertTrue(accuracy_test.get(0).intValue() == john.getAccuracy());
+		assertTrue(accuracy_test.get(1).intValue() == james.getAccuracy());
+	}
+
+	@Test
+	public void getAllPlayerTimesReturnsCorrectValue() {
+		 array_list = new Players();
+		 john = new Player("John");
+		 james = new Player("James");
+
+		array_list.addPlayer(john);
+		array_list.addPlayer(james);
+
+		ArrayList<Integer> accuracy_test = array_list.getAllPlayersAccuracies();
+		assertTrue(accuracy_test.get(0).intValue() == john.getAverageTime());
+		assertTrue(accuracy_test.get(1).intValue() == james.getAverageTime());
+	}
+
+	@Test
+	public void getAllCryptogramsPlayedReturnsCorrectValue() {
+		 array_list = new Players();
+		 john = new Player("John");
+		 james = new Player("James");
+
+		array_list.addPlayer(john);
+		array_list.addPlayer(james);
+
+		ArrayList<Integer> accuracy_test = array_list.getAllPlayersAccuracies();
+		assertTrue(accuracy_test.get(0).intValue() == john.getNumCryptogramsPlayed());
+		assertTrue(accuracy_test.get(1).intValue() == james.getNumCryptogramsPlayed());
+	}
+
+	@Test
+	public void getAllCompletedCryptogramsReturnsCorrectValue() {
+		 array_list = new Players();
+		 john = new Player("John");
+		 james = new Player("James");
+
+		array_list.addPlayer(john);
+		array_list.addPlayer(james);
+
+		ArrayList<Integer> accuracy_test = array_list.getAllPlayersAccuracies();
+		assertTrue(accuracy_test.get(0).intValue() == john.getNumCryptogramsCompleted());
+		assertTrue(accuracy_test.get(1).intValue() == james.getNumCryptogramsCompleted());
 	}
 
 }
